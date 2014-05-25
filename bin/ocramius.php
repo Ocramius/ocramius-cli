f198df56fa20d4a46ece30582bdd5948ab9ffce7
Marco Pivetta <ocramius@gmail.com>
2014-05-25 05:31:16 +0200

    Uploading data to gist.github.com

--- bin/ocramius.php
+++ bin/ocramius.php
@@ -4,7 +4,12 @@
 use Ocramius\Console\Command\Help;
 use Ocramius\Console\Symbol\Goodbye;
 use Ocramius\Console\Symbol\Logo;
+use Ocramius\Persister\File;
+use Ocramius\Persister\Gist;
+use Zend\Console\ColorInterface;
 use Zend\Console\Console;
+use Zend\Console\Prompt\Confirm;
+use Zend\Http\Client;
 
 $files = [
     __DIR__ . '/../vendor/autoload.php',
@@ -25,15 +30,34 @@ if (! isset($loader)) {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
 }
 
-$console = Console::getInstance();
-$logo    = new Logo();
-$help    = new Help();
-$goodbye = new Goodbye();
+$filePersister = new File(getcwd());
+$gistPersister = new Gist(new Client());
+$console       = Console::getInstance();
+$logo          = new Logo();
+$help          = new Help();
+$goodbye       = new Goodbye();
 
 $logo->draw($console);
 
 $report = $help->help($console);
 
-$goodbye->draw($console);
+$file = $filePersister->persist($report);
 
-var_dump($report);
+$console->writeLine('The information has been saved to', ColorInterface::GREEN);
+$console->writeLine(realpath($file), ColorInterface::BLACK, ColorInterface::CYAN);
+
+$console->write('May I upload this information on https://gist.github.com/ ?', ColorInterface::CYAN);
+$console->writeLine('[y/n]', ColorInterface::GREEN);
+
+$prompt = new Confirm('');
+
+$prompt->setConsole($console);
+
+if ($prompt->show()) {
+    $gistUri = $gistPersister->persist($report);
+
+    $console->writeLine('The information was uploaded to', ColorInterface::GREEN);
+    $console->writeLine($gistUri, ColorInterface::BLACK, ColorInterface::CYAN);
+}
+
+$goodbye->draw($console);
